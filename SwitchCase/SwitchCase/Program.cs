62bd0541d815a678f30e45aa4127df4e2c9aad67
zuleyha04 <zuleyhakbas04@gmail.com>
2024-02-13 23:44:18 +0300

    SwitchCase
    İf/else ile yaptığım "mevsim" örneğinin switch'e çevrilmiş hali

---
+++ SwitchCase/SwitchCase/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mevsim;
+            Console.WriteLine("Lütfen mevsimi küçük harflerle yazınız : ");
+            mevsim = Console.ReadLine();
+
+            switch (mevsim)
+            {
+                case "yaz":Console.WriteLine("Yaz mevsimi ayları : Haziran Temmuz Ağustos ");
+                    break;
+                case "ilkbahar":Console.WriteLine("İlkbahar mevsimi ayları : Mart Nisan Mayıs ");
+                    break;
+                case "sonbahar":Console.WriteLine("Sonbahar mevsimi ayları : Eylül Ekim Kasım");
+                    break;
+                case "kis":Console.WriteLine("Kış mevsimi ayları : Aralık Ocak Şubat ");
+                    break;
+                default:Console.WriteLine("Hatalı mevsim girişi...");
+                    break;
+            }
+            Console.Read();
+        }
+    }
+}
