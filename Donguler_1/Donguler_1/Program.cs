89be8bae0a55e4b2d2528ddd1224a89c9824b761
zuleyha04 <zuleyhakbas04@gmail.com>
2024-02-14 09:50:20 +0300

    Donguler_1
    "for "örnekleri

---
+++ Donguler_1/Donguler_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Donguler_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1den 10a yazdırma
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+
+            //1den 10a kadar sayıların toplamı
+            int toplam = 0;
+
+            for (int i = 0; i <= 10; i++)
+            {
+                toplam = toplam + i;
+                Console.WriteLine(toplam);
+            }
+            Console.ReadLine();
+
+
+            //çift sayıların toplamı 
+            int topl = 0;
+
+            for (int i = 0; i <= 10; i+=2)
+            {
+                topl = topl + i;
+                Console.WriteLine("Toplam : "+ topl);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
