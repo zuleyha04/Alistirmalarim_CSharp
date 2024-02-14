39868bf2b4eebea0d1b1ca1834f38e394cf26081
zuleyha04 <zuleyhakbas04@gmail.com>
2024-02-14 09:57:28 +0300

    Donguler_3
    "while" örneği

---
+++ Donguler_3/Donguler_3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Donguler_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("i'nin döngüden çıkış değeri : " + i);
+
+
+
+
+
+            //faktoriyel
+            int say,faktoriyel = 1;
+            Console.Write("***** Faktoriyeli alınacak sayıyı giriniz :  ");
+            say = Convert.ToInt32(Console.ReadLine());
+            
+
+            while (say>0)
+            {
+                faktoriyel = faktoriyel * say;
+                say--;
+               
+            }
+            Console.WriteLine("Faktoriyel : "+faktoriyel);
+            Console.ReadLine();
+        }
+        
+    }
+
+}
+
